1757b8d0739c79188d61660c69d28a713388ceb6
EricLiu750501 <qazwsxedc123456789eric@gmail.com>
2026-03-11 18:04:54 +0800

    redefine image.nvim rendering mode

--- lua/plugins/image.lua
+++ lua/plugins/image.lua
@@ -1,4 +1,11 @@
 return {
   "3rd/image.nvim",
-  opts = {},
+  opts = {
+      integrations = {
+    markdown = {
+        only_render_image_at_cursor = true, -- defaults to false
+        only_render_image_at_cursor_mode = "popup", -- "popup" or "inline", defaults to "popup"
+    }
+  },
+}
 }
